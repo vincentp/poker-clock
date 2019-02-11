cbbe0e22a9b15b0d5c0064958aa8e697c0cc9b36
Vincent Peres <vincent.peres@gmail.com>
2019-02-11 10:36:33 +0100

    Store state to localStorage

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { loadState, saveState } from './localStorage'
 
 export default function configureStore() {
-  return createStore(
+  const persistedState = loadState();
+
+  const store = createStore(
     rootReducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
   );
+
+  store.subscribe(() => {
+    saveState(store.getState());
+  });
+
+  return store;
 }
