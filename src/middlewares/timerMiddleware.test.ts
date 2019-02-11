2b6be41ec79d819eb1bc2f3b07dea2cd9d74226c
Vincent Peres <vincent.peres@gmail.com>
2019-02-11 15:31:52 +0100

    Adjust logic and add test to move up timers

---
+++ src/middlewares/timerMiddleware.test.ts
@@ -0,0 +1,77 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import Clock from "./clock";
+import { mount } from "../enzyme";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import { TICK_CLOCK, NEXT_TIMER } from "../actions/actionTypes";
+import thunk from "redux-thunk";
+import { tickActiveTimer } from "./timerMiddleware"
+
+// @ts-ignore
+import { JssProvider } from "react-jss";
+
+describe("Timer middleware Component", () => {
+  const oneTimerState = { 
+    clock: {
+      activeTimer: 0,
+      timers: [{ secondsLeft: 60, minutes: 1 }] 
+    } 
+  };
+
+  const multipleTimersState = { 
+    clock: {
+      activeTimer: 1,
+      timers: [
+        { secondsLeft: 60 },
+        { secondsLeft: 60 },
+        { secondsLeft: 60 }
+      ] 
+    } 
+  };
+
+  const mockStore = configureStore([thunk]);
+  let store: any;
+
+  it("should tick the clock", () => {
+    store = mockStore(oneTimerState);
+    store.dispatch(tickActiveTimer());
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: TICK_CLOCK });
+  });
+
+  it("should not go to the next timer if the current one has seconds left", () => {
+    store = mockStore(oneTimerState);
+    store.dispatch(tickActiveTimer());
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({ type: TICK_CLOCK });
+    expect(actions[1]).toEqual(undefined);    
+  });
+
+  it("should not go to the next timer if it's the last one", () => {
+    let state = {...multipleTimersState};
+    state.clock.activeTimer = 2;
+    state.clock.timers[0].secondsLeft = 0;
+    state.clock.timers[1].secondsLeft = 0;
+    state.clock.timers[2].secondsLeft = 0;
+    store = mockStore(state);
+    const actions = store.getActions();
+    store.dispatch(tickActiveTimer());
+    expect(actions[0]).toEqual({ type: TICK_CLOCK });
+    expect(actions[1]).toEqual(undefined);    
+  });
+
+  it("should go to the next timer if there if it's not the last one", () => {
+    let state = {...multipleTimersState};
+    state.clock.activeTimer = 1;
+    state.clock.timers[0].secondsLeft = 0;
+    state.clock.timers[1].secondsLeft = 0;
+    state.clock.timers[2].secondsLeft = 60;
+    store = mockStore(state);
+    const actions = store.getActions();
+    store.dispatch(tickActiveTimer());
+    expect(actions[0]).toEqual({ type: TICK_CLOCK });
+    expect(actions[1]).toEqual({ type: NEXT_TIMER });    
+  });
+
+});
--- src/middlewares/timerMiddleware.ts
+++ src/middlewares/timerMiddleware.ts
@@ -4,8 +4,11 @@ import { Timer } from "../common/types";
 export const tickActiveTimer = () => {
   return (dispatch: any, getState: any) => {
     dispatch({ type: TICK_CLOCK });
-    if (getState().clock.timers[getState().clock.activeTimer].secondsLeft === 0)
+
+    if (
+      getState().clock.timers[getState().clock.activeTimer].secondsLeft === 0 &&
+      getState().clock.timers[getState().clock.activeTimer + 1] !== undefined
+    )
       dispatch({ type: NEXT_TIMER });
   };
 };
-
